1842ca5be6b704e544df8f27380a051e9f2c3ed4
David Cloutier <dacloutier@users.noreply.github.com>
2025-05-21 12:03:42 +0200

    Part 2

--- src/main/kotlin/rpg/combat/makers/Character.kt
+++ src/main/kotlin/rpg/combat/makers/Character.kt
@@ -5,4 +5,12 @@ class Character(
     var level: Int = 1,
 ) {
     fun alive(): Boolean = health > 0
+
+    fun attack(defender: Character, damage: Int) {
+        defender.receiveDamage(damage)
+    }
+
+    fun receiveDamage(damage: Int) {
+        health -= Math.min(damage, health)
+    }
 }
--- src/test/kotlin/rpg/combat/makers/CharacterTest.kt
+++ src/test/kotlin/rpg/combat/makers/CharacterTest.kt
@@ -1,5 +1,6 @@
 package rpg.combat.makers
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -31,4 +32,38 @@ class CharacterTest {
 
         assertEquals(character.alive(), true)
     }
+
+    @Nested
+    inner class Part2 {
+        @Test
+        fun `when attacked damage is subtracted from health`() {
+            val attacker = Character()
+            val defender = Character()
+
+            attacker.attack(defender, 100)
+
+            assertEquals(defender.health, 900)
+        }
+
+        @Test
+        fun `when damage is greater or equal to the remaining health character dies` () {
+            val attacker = Character()
+            val defender = Character()
+
+            attacker.attack(defender, 2000)
+
+            assertEquals(defender.alive(), false)
+        }
+
+        @Test
+        fun `character health should never be lower than 0` () {
+            val attacker = Character()
+            val defender = Character()
+
+            attacker.attack(defender, 2000)
+
+            assertEquals(defender.health, 0)
+        }
+    }
+
 }
